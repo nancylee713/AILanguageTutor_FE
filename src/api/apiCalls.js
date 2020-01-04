ed201d6e37e3b0556804b7e0382117eaabaad3f0
Noah Gibson <noah.gibson99@gmail.com>
2020-01-04 13:43:10 -0700

    Add fetch for perfect tense

--- src/api/apiCalls.js
+++ src/api/apiCalls.js
@@ -2,8 +2,26 @@ export const getQuestions = async () => {
 
 };
 
-export const reviewGrammar = async () => {
-  // the headers will need to contain two api keys, in this case they will both be the PERFECT_TENSE_TEY environment variable
-  
+export const reviewGrammar = async (userSentence) => {
+  const url = 'https://api.perfecttense.com/correct';
+  const body = { "text": userSentence, "responseType": ["corrected", "grammarScore", "rulesApplied", "offset", "summary"] };
+  const options = {
+    method: 'POST',
+    headers: { 
+      'Content-Type': 'application/json', 
+      'Authorization': process.env.PERFECT_TENSE_KEY, 
+      'AppAuthorization': process.env.PERFECT_TENSE_APP_AUTH },
+      body: JSON.stringify(body)
+  };
+  try {
+    const response = await fetch(url, options)
+    if (!response.ok) {
+      throw new Error('There was an error in data analysis.');
+    }
+    const correctedSentence = response.json();
+    return correctedSentence;
+  } catch (error) {
+    throw new Error(error);
+  }
 };
 
