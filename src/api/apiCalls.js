aa5caf06ddebd3917d0c3591151d6c8056414587
Noah Gibson <noah.gibson99@gmail.com>
2020-01-03 23:55:25 -0700

    Add api key variable

--- src/api/apiCalls.js
+++ src/api/apiCalls.js
@@ -3,6 +3,7 @@ export const getQuestions = async () => {
 };
 
 export const reviewGrammar = async () => {
+  // the headers will need to contain two api keys, in this case they will both be the PERFECT_TENSE_TEY environment variable
   
 };
 
