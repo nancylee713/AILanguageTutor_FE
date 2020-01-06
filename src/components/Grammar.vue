919355b32ee9a8dcf4e4611cda3f816be7f88989
Noah Gibson <noah.gibson99@gmail.com>
2020-01-06 15:14:03 -0700

    Render scores conditionally

--- src/components/Grammar.vue
+++ src/components/Grammar.vue
@@ -6,12 +6,16 @@
       <!-- <p>Question number: {{ qnum }}</p> -->
     </div>
     <div class='content'>
-      <button>Play</button>
       <p>Type out the sentence</p>
       <div class='grammar-input'>
         <input v-model='userInput' type='text'/>
         <button v-on:click='checkGrammar'>Submit</button>
         <p>{{ userInput }}</p>
+      </div>
+      <div v-if='correctedGrammar' class='feedback'>
+        <p>Grammar Score:{{ correctedGrammar.grammarScore }}</p>
+        <p>Corrected Sentence: {{ correctedGrammar.corrected }}</p>
+        <p></p>
       </div>
     </div>
     <div class='next'>
@@ -36,7 +40,8 @@ export default {
   methods: {
     checkGrammar: function () {
       reviewGrammar(this.userInput)
-        .then(data => this.correctedGrammar = data);
+        .then(data => this.correctedGrammar = data)
+        .then(data => console.log(data))
     },
   },
 };
