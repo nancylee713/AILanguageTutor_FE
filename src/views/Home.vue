fc1aeb8bfd2b2eb9f430104d990b8cb675fe40c3
Noah Gibson <noah.gibson99@gmail.com>
2020-01-06 23:22:00 -0700

    Add basic style to home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <h1>AI Language Learner</h1>
-    <h2>Welcome to AI Language Learner where we are commited to assisting your language learning needs!</h2>
+    <h2 class='welcome'>Welcome to AI Language Learner where we are commited to assisting your language learning needs!</h2>
   </div>
 </template>
 
@@ -12,3 +12,16 @@ export default {
   },
 };
 </script>
+
+<style>
+  h1 {
+    font-size: 2em;
+    margin: 40px;
+  }
+
+  .welcome  {
+    font-size: 1.5em;
+    width: 80%;
+    margin: auto;
+  }
+</style>
