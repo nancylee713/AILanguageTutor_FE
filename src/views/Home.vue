016600ac73c7d4f6f89d72c22c4770cb2e6e0131
Scott <swschipke@gmail.com>
2020-01-08 18:17:07 -0700

    Merge current

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,18 @@
   <div class="home">
     <h1>AI Language Learner</h1>
     <h2 class='welcome'>Welcome to AI Language Learner where we are commited to assisting your language learning needs!</h2>
+      <audio-recorder
+    upload-url="YOUR_API_URL"
+    :attempts="3"
+    :time="2"
+    :headers="headers"
+    :before-recording="callback"
+    :pause-recording="callback"
+    :after-recording="callback"
+    :select-record="callback"
+    :before-upload="callback"
+    :successful-upload="callback"
+    :failed-upload="callback"/>
   </div>
 </template>
 
@@ -11,6 +23,7 @@ export default {
   components: {
   },
 };
+import AudioRecorder from 'vue-audio-recorder'
 </script>
 
 <style>
