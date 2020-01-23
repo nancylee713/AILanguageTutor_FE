a1ba0cb80122c8b66a3cf06043bb482e1112a87c
Scott <swschipke@gmail.com>
2020-01-22 20:33:37 -0700

    Obtain user information from form

--- src/components/NewUserForm.vue
+++ src/components/NewUserForm.vue
@@ -1,25 +1,53 @@
 <template>
-    <section class='signup-form'>
+    <form @submit.prevent="handleSubmit" class='signup-form'>
         <b-field label="Name">
-            <b-input class='input-field' value=""></b-input>
+            <b-input class='input-field' required v-model="account.name" value="" maxlength="40" placeholder="Please enter your name"></b-input>
         </b-field>
 
         <b-field label="Email">
             <b-input class='input-field' type="email"
+                required
+                v-model="account.email"
                 value=""
                 maxlength="40">
             </b-input>
         </b-field>
         <b-field label="Username">
-            <b-input class='input-field' value="" maxlength="20"></b-input>
+            <b-input class='input-field' required value="" v-model="account.username" maxlength="20"></b-input>
         </b-field>
 
         <b-field label="Password">
-            <b-input class='input-field' value="" type="password" maxlength="30"></b-input>
+            <b-input class='input-field' required value="" v-model="account.password" type="password" maxlength="30"></b-input>
         </b-field>
-        <b-button type="is-primary" outlined>Submit</b-button>
-    </section>
+        <button type="submit" outlined>Submit</button>
+    </form>
 </template>
+
+<script>
+
+export default {
+  name: 'sign up form',
+  data() {
+    return {
+        account: {
+            name: null,
+            username: null,
+            email: null,
+            password: null
+        }
+    }
+  },
+  methods: {
+    handleSubmit(){
+        console.log({...this.account})
+    }
+},
+  mounted() {
+
+  }
+}
+
+</script>
 
 <style lang='scss'>
     .signup-form {
